05945b45b9bda3e073d5200be46282453e2b01b7
Ben <benzoic09@gmail.com>
2024-04-24 23:51:25 +0300

    add utils

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
