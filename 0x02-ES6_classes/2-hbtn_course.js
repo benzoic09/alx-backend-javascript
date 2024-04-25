6153d795b9fb0c032c1c3c203bb75b5eaa57efeb
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-04-25 17:10:41 +0300

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+export default class HolbertonCourse {
+  constructor(name, length, student) {
+    this.name = name;
+    this.length = length;
+    this.student = student;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an Array');
+    }
+  }
+}
