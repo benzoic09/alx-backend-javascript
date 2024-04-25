1c8ced5fe2f97cdfe8cbd1030a180bc1fc57f2ef
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-04-25 23:24:05 +0300

    task 3

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -34,7 +34,6 @@ export default class HolbertonCourse {
   set students(students) {
     if(!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
       this._students = students;
-    } else {
       throw new TypeError('Students must be an Array');
     }
     this._students = students;
