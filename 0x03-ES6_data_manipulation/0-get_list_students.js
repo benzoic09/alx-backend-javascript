5ba85444a487f2b7383e061ca421395b3d8a983b
Ben <benzoic09@gmail.com>
2024-04-29 21:01:13 +0300

    task zero

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  const stu1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const stu2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const stu3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [stu1, stu2, stu3];
+}
