c4a4767d7cd95bc5003b9a389c8a0f3dca529197
Ben <benzoic09@gmail.com>
2024-04-30 14:30:12 +0300

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const filteredStudents = students.filter((student) => student.location === city);
   return filteredStudents.map((student) => {
-    const newGrade = newGrades.filter((grade) => grade.studentid === student.id);
-    if (newGrade.length > 0) {
-      return {
-        ...student, grade: newGrade[0].grade,
-      };
+    const newGrade = newGrades.find((grade) => grade.studentid === student.id);
+    if (newGrade) {
+      return { ...student, grade: newGrade.grade };
     }
-    return {
-      ...student, grade: 'N/A',
-    };
+    return { ...student, grade: 'N/A' };
   });
 }
