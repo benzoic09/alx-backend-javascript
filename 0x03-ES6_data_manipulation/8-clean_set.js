56d0d3756946fdc37c2f9bf3114a0d4d4cc82e57
Ben <benzoic09@gmail.com>
2024-04-30 13:43:26 +0300

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
 export default function cleanSet(set, startString) {
   const filteredValues = Array.from(set).filter((value) => value.startWith(startString));
-  return filteredValues.join('_');
+  return filteredValues.join('-');
 }
