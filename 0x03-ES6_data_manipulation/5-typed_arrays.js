35a32ee691f7999b6f7bdc8dc6e9784ca4161157
Ben <benzoic09@gmail.com>
2024-04-30 12:01:23 +0300

    task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,17 +1,16 @@
 export default function createInt8TypedArray(length, position, value) {
-  // Check if the position is within the range of the ArrayBuffer
-  if (position >= length) {
-    throw new Error('Position outside range');
-  }
-
   // Create a new ArrayBuffer with the specified length
   const buffer = new ArrayBuffer(length);
 
   // Create an Int8Array view on the buffer
   const int8Array = new Int8Array(buffer);
 
+  if (position > int8Array) {
+    throw new Error('Position outside range');
+  }
+
   // Set the value at the specified position
   int8Array[position] = value;
 
-  return buffer;
+  return new DataView(buffer);
 }
