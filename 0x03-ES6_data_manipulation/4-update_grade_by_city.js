75afe089f0af19087685920b4fabb09ed8eeeec8
Ben <benzoic09@gmail.com>
2024-04-30 11:51:27 +0300

    task 5

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const filteredStudents = students.filter((student) => student.location === city);
   return filteredStudents.map((student) => {
-    const newGrade = newGrades.find((grade) => grade.id === student.id);
+    const newGrade = newGrades.filter((grade) => grade.studentid === student.id);
     if (newGrade.length > 0) {
       return {
         ...student, grade: newGrade[0].grade,
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   // Check if the position is within the range of the ArrayBuffer
-  if (position < 0 || position >= length) {
+  if (position >= length) {
     throw new Error('Position outside range');
   }
 
