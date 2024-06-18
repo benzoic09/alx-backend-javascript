4363737c11e067a45fbc94ab0713369711807bcd
Ben Kimani <benzoic09@gmail.com>
2024-06-18 14:15:37 +0300

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?')
+
+r1.on('line', (input)=>{
+    console.log(`Your name is: ${input}`)
+    r1.close();
+});
+
+r1.on('close', () => {
+    console.log('This important software is now closing');
+});
