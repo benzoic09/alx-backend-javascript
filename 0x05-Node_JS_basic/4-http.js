fd41a0da7278fd520c8607383e3ead920641ea89
Ben Kimani <benzoic09@gmail.com>
2024-06-19 15:47:02 +0300

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,17 +2,17 @@ const http = require('http');
 
 // Create an HTTP server
 const app = http.createServer((req, res) => {
-    // Set the response HTTP header with HTTP status and Content-Type
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
-    // Send the response body as "Hello Holberton School!"
-    res.end('Hello Holberton School!\n');
+  // Set the response HTTP header with HTTP status and Content-Type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the response body as "Hello Holberton School!"
+  res.end('Hello Holberton School!\n');
 });
 
 // Start the HTTP server and listen on port 1245
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server is running and listening on port ${PORT}`);
+  console.log(`Server is running and listening on port ${PORT}`);
 });
 
 // Export the app variable for external use
