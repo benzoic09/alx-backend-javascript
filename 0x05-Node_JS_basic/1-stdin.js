87776a837931554cced5e9049b88543cafb18856
Ben Kimani <benzoic09@gmail.com>
2024-06-19 09:49:05 +0300

    update task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,13 @@
-const readline = require('readline');
+// #!/usr/bin/env node
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   // Interactive mode
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.question('', (name) => {
-    process.stdout.write(`Your name is: ${name}\n`);
-    rl.close();
+  process.stdin.on('data', (name) => {
+    process.stdout.write(`Your name is: ${name.toString().trim()}\n`);
+    //process.stdout.write('This important software is now closing\n');
+    process.exit();
   });
 } else {
   // Non-interactive mode
