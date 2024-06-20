9b7528c46d4d9ce484835806fa6143a48301874a
Ben Kimani <benzoic09@gmail.com>
2024-06-20 14:17:08 +0300

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,24 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function(){
-    it('should return 4 when inputs are 1 and 3', function(){
-        assert.strictEqual(calculateNumber(1, 3), 4);
-    });
+describe('calculateNumber', () => {
+  it('should return 4 when inputs are 1 and 3', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
 
-    it('should return 5 when inputs are 1.4 and 3.6', function() {
+  it('should return 5 when inputs are 1.4 and 3.6', () => {
     assert.strictEqual(calculateNumber(1.4, 3.6), 5);
-    });
+  });
 
-    it('should return 6 when inputs are 1.5 and 4.5', function() {
+  it('should return 6 when inputs are 1.5 and 4.5', () => {
     assert.strictEqual(calculateNumber(1.5, 4.5), 7);
-    });
+  });
 
-    it('should return 6 when inputs are 1.4 and 4.6', function() {
+  it('should return 6 when inputs are 1.4 and 4.6', () => {
     assert.strictEqual(calculateNumber(1.4, 4.6), 6);
-    });
+  });
 
-    it('should return 0 when inputs are 0 and 0', function() {
-        assert.strictEqual(calculateNumber(0, 0), 0);
-      });
+  it('should return 0 when inputs are 0 and 0', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
 });
