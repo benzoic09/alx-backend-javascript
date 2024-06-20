6cf2132b6c7b5737d0900fa809f8d769c66275cc
Ben Kimani <benzoic09@gmail.com>
2024-06-20 16:47:47 +0300

    eslint

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,18 +1,17 @@
 function calculateNumber(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-    if (type === 'SUM') {
-      return roundedA + roundedB;
-    } if (type === 'SUBTRACT') {
-      return roundedA - roundedB;
-    } if (type === 'DIVIDE') {
-      if (roundedB === 0) {
-        return 'Error';
-      }
-      return roundedA / roundedB;
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+  if (type === 'SUM') {
+    return roundedA + roundedB;
+  } if (type === 'SUBTRACT') {
+    return roundedA - roundedB;
+  } if (type === 'DIVIDE') {
+    if (roundedB === 0) {
+      return 'Error';
     }
-    throw new Error('Invalid operation type');
+    return roundedA / roundedB;
   }
-  
-  module.exports = calculateNumber;
-  
+  throw new Error('Invalid operation type');
+}
+
+module.exports = calculateNumber;
