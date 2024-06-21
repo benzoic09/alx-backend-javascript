bbc3661f5c77ca4706a17b48a810793a44734241
Ben Kimani <benzoic09@gmail.com>
2024-06-21 17:08:59 +0300

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,44 +1,44 @@
 const http = require('http');
-const fs = require('fs');
 const path = require('path');
-const { countStudents } = require('./3-read_file_async'); // Importing the countStudents function
+const countStudents = require('./3-read_file_async'); // Importing the countStudents function
 
 // Define the path to the database CSV file
-const databasePath = path.join(__dirname, 'database.csv');
+const databasePath = process.argv[2] || path.join(__dirname, 'database.csv');
 
 // Create an HTTP server
 const app = http.createServer((req, res) => {
-    // Determine the URL path
-    const url = req.url;
+  // Determine the URL path
+  const { url } = req;
 
-    // Set the response HTTP header with HTTP status and Content-Type
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+  // Set the response HTTP header with HTTP status and Content-Type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-    // Handling different URL paths
-    if (url === '/') {
-        // Endpoint: /
-        res.end('Hello Holberton School!\n');
-    } else if (url === '/students') {
-        // Endpoint: /students
-        // Call the countStudents function asynchronously
-        countStudents(databasePath)
-            .then((result) => {
-                res.end(`This is the list of our students\n${result}`);
-            })
-            .catch((error) => {
-                res.end(`Cannot load the database\n${error}`);
-            });
-    } else {
-        // Handle other endpoints
-        res.end('Endpoint not found\n');
-    }
+  // Handling different URL paths
+  if (url === '/') {
+    // Endpoint: /
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    // Endpoint: /students
+    // Call the countStudents function asynchronously
+    countStudents(databasePath)
+      .then((result) => {
+        res.end(`This is the list of our students\n${result}`);
+      })
+      .catch(() => {
+        res.end('Cannot load the database\n');
+      });
+  } else {
+    // Handle other endpoints
+    res.statusCode = 404;
+    res.end('Endpoint not found\n');
+  }
 });
 
 // Start the HTTP server and listen on port 1245
 const PORT = 1245;
 app.listen(PORT, () => {
-    //console.log(`Server is running and listening on port ${PORT}`);
+  console.log(`Server is running and listening on port ${PORT}`);
 });
 
 // Export the app variable for external use
